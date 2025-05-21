11b8d34b1289946d4dc591886a10bedba892c3f5
Philipp Zinniel <zinniel.philipp@gmail.com>
2025-05-21 17:40:56 +0200

    removed default styling for buttons added table for schienenabschnitte

--- src/components/kunden-form/KundenForm.jsx
+++ src/components/kunden-form/KundenForm.jsx
@@ -1,74 +1,90 @@
 import './KundenForm.css'
-
-const TEST_DATA = [
-    {
-        schienentyp: "UIC60",
-        schienenhaerte: 260,
-        maximale_geschwindigkeit: 160,
-        laenge: 2.5,
-        id: 1
-    },
-    {
-        schienentyp: "S54",
-        schienenhaerte: 240,
-        maximale_geschwindigkeit: 120,
-        laenge: 1.8,
-        id: 2
-    }
-]
+import {useState} from "react";
 
 function KundenForm() {
-    const abschnittRows = TEST_DATA.map(abschnitt =>
-        <tr key={abschnitt.id}>
-            <td>{abschnitt.schienentyp}</td>
-            <td>{abschnitt.schienenhaerte}</td>
-            <td>{abschnitt.maximale_geschwindigkeit}</td>
-            <td>{abschnitt.laenge}</td>
+    function uploadData(formData) {
+        console.log(formData);
+    }
+
+    const [rows, setRows] = useState([
+        {schienentyp: "", schienenhaerte: "", maximale_geschwindigkeit: "", laenge: ""}
+    ]);
+
+    const addRow = () => {
+        setRows([
+            ...rows,
+            {schienentyp: "", schienenhaerte: "", maximale_geschwindigkeit: "", laenge: ""}
+        ]);
+    };
+
+    const abschnittRows = rows.map((abschnitt, index) =>
+        <tr>
+            <td>
+                <input name={"schienentyp" + index} className="table-input" type="text" defaultValue={abschnitt.schienentyp}/>
+            </td>
+            <td>
+                <input name={"schienenhaerte" + index} className="table-input" type="number" defaultValue={abschnitt.schienenhaerte}/>
+            </td>
+            <td>
+                <input name={"maximale_geschwindigkeit" + index} className="table-input" type="number" defaultValue={abschnitt.maximale_geschwindigkeit}/>
+            </td>
+            <td>
+                <input name={"laenge" + index} className="table-input" type="number" defaultValue={abschnitt.laenge}/>
+            </td>
         </tr>
     );
 
     return (
         <div className="kunden-eingabe">
             <h2>Kunden hinzufügen</h2>
-            <form className="kunden-form">
+            <form className="kunden-form" action={uploadData}>
 
                 <div className="kunden-inputs">
-                    <span className="input-field input-full">
-                        <label htmlFor="name">Name</label>
-                        <input name="name" type="text" placeholder="Name"/>
-                    </span>
+                    <div className="input-group input-full">
+                        <input className="input-field" id="name" name="name" type="text" placeholder="Name"/>
+                        <label className="input-label" htmlFor="name">Name</label>
+                    </div>
 
-                    <span className="input-field input-small">
-                        <label htmlFor="email">Email</label>
-                        <input name="email" type="email" placeholder="Email"/>
-                    </span>
+                    <div className="input-group input-small">
+                        <input className="input-field" id="email" name="email" type="email" placeholder="Email"/>
+                        <label className="input-label" htmlFor="email">Email</label>
+                    </div>
 
-                    <span className="input-field input-small">
-                        <label htmlFor="telefon">Telefon</label>
-                        <input name="telefon" type="text" placeholder="Telefon"/>
-                    </span>
+                    <div className="input-group input-small">
+                        <input className="input-field" id="telefon" name="telefon" type="text" placeholder="Telefon"/>
+                        <label className="input-label" htmlFor="telefon">Telefon</label>
+                    </div>
 
-                    <span className="input-field input-full">
-                        <label htmlFor="adresse">Adresse</label>
-                        <input name="adresse" type="text" placeholder="Adresse"/>
-                    </span>
+                    <div className="input-group input-full">
+                        <input className="input-field" id="adresse" name="adresse" type="text" placeholder="Adresse"/>
+                        <label className="input-label" htmlFor="adresse">Adresse</label>
+                    </div>
                 </div>
 
-                <table className="schienenabschnitt-liste">
-                    <thead>
-                    <tr>
-                        <th>Schienentyp</th>
-                        <th>Schienenhärte</th>
-                        <th>Maximale Geschwindigkeit</th>
-                        <th>Länge</th>
-                    </tr>
-                    </thead>
-                    <tbody>
+                <div className="schienenabschnitt-table">
+                    <table className="schienenabschnitt-liste">
+                        <thead>
+                        <tr className="table-header">
+                            <th>Schienentyp</th>
+                            <th>Schienenhärte</th>
+                            <th>Maximale Geschwindigkeit</th>
+                            <th>Länge</th>
+                        </tr>
+                        </thead>
+                        <tbody>
                         {abschnittRows}
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
 
-                <input name="submit" type="submit" value="Hinzufügen"/>
+                <div className="button-group">
+                    <button className="add-schienenabschnitt-button" onClick={addRow} type="button">
+                        Neuen Abschnitt hinzufügen
+                    </button>
+
+                    <input name="submit" type="submit" className="save-button" value="Hinzufügen"/>
+                </div>
+
             </form>
         </div>
     )
